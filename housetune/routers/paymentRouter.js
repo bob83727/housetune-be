a93f8f902f971356e8c12390eba863eb17a46a6d
Hikariyen881230 <hikariyen881230@gmail.com>
2023-02-01 14:40:10 +0800

    結帳後頁面取得訂單編號

--- housetune/routers/paymentRouter.js
+++ housetune/routers/paymentRouter.js
@@ -25,12 +25,22 @@ router.post('/', async (req, res, next) => {
       'INSERT INTO order_detail (order_list_id,product_id) VALUES (?,?);',
       [result[0].insertId, JSON.stringify(productData)]
     )
-    console.log('result', result)
-    console.log('result2', result2)
+    // console.log('result', result)
+    // console.log('result2', result2)
     res.json('新增成功')
   } catch (err) {
     res.json('新增失敗', err)
   }
+})
+// 訂單新增後取得該訂單編號
+router.get('/checkorder', async (req, res, next) => {
+  // 拿到該使用者的訂單
+  console.log('POST/api/payment/checkorder', req.session.member.id)
+  let [result] = await pool.query(
+    'SELECT * FROM order_list WHERE user_id =? ORDER BY ordL_id DESC LIMIT 0 , 1;',
+    [req.session.member.id]
+  )
+  res.json(result[0].ordL_id)
 })
 
 module.exports = router
