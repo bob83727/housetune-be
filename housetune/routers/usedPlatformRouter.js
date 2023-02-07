032ac3a2cdc4213899926305f427c7197657d836
Hikariyen881230 <hikariyen881230@gmail.com>
2023-02-07 22:06:00 +0800

    修改二手商品的SQL

--- housetune/routers/usedPlatformRouter.js
+++ housetune/routers/usedPlatformRouter.js
@@ -4,9 +4,20 @@ const pool = require('../utils/db')
 
 router.get('/usedproducts', async (req, res) => {
   let results = await pool.query(
-    'SELECT u.useP_id, u.seller_id,u.category_product,u.amount,u.description,u.price,u.img,u.bought_in,u.style,u.updated_at,u.valid,u.name AS product_name ,user.user_id, user.cart, user.name, user.liked,user.rating FROM used_product AS u LEFT JOIN user on u.seller_id = user.user_id '
+    'SELECT u.useP_id, u.seller_id,u.category_product,u.amount,u.description,u.price,u.img,u.bought_in,u.updated_at,u.valid,u.name AS product_name,u.description,user.user_id, user.cart, user.name, user.liked,user.rating,user.account FROM used_product AS u LEFT JOIN user on u.seller_id = user.user_id '
   )
   let data = results[0]
+  res.json(data)
+})
+
+router.get('/usedproduct/:usedProdId', async (req, res) => {
+  console.log('req.params.usedProdId', req.params)
+  let result = await pool.query(
+    'SELECT u.useP_id, u.seller_id,u.category_product,u.amount,u.description,u.price,u.img,u.bought_in,u.updated_at,u.valid,u.name AS product_name ,user.user_id, user.cart, user.name, user.liked,user.rating,user.account FROM used_product AS u LEFT JOIN user on u.seller_id = user.user_id WHERE useP_id=?',
+    [req.params.usedProdId]
+  )
+  let data = result[0]
+  console.log(data)
   res.json(data)
 })
 
