7f3f6bf27e838d8c5cd4ed108f5e1cc700e4b260
Hikariyen881230 <hikariyen881230@gmail.com>
2023-02-07 15:31:41 +0800

    更新二手商品router

--- housetune/routers/usedPlatformRouter.js
+++ housetune/routers/usedPlatformRouter.js
@@ -3,9 +3,11 @@ const router = express.Router()
 const pool = require('../utils/db')
 
 router.get('/usedproducts', async (req, res) => {
-    let results = await pool.query('SELECT u.useP_id, u.seller_id,u.category_product,u.amount,u.description,u.price,u.img,u.bought_in,u.style,u.updated_at,u.valid,u.name AS product_name ,user.user_id, user.cart, user.name, user.liked,user.rating FROM used_product AS u LEFT JOIN user on u.seller_id = user.user_id ')
-    let data = results[0]; 
-    res.json(data)
-    })
+  let results = await pool.query(
+    'SELECT u.useP_id, u.seller_id,u.category_product,u.amount,u.description,u.price,u.img,u.bought_in,u.style,u.updated_at,u.valid,u.name AS product_name ,user.user_id, user.cart, user.name, user.liked,user.rating FROM used_product AS u LEFT JOIN user on u.seller_id = user.user_id '
+  )
+  let data = results[0]
+  res.json(data)
+})
 
 module.exports = router
